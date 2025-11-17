349c1149bf899563987f9c1bb11423cdbfbd539c
Geovanni Magnani <magnanigeovanni@gmail.com>
2025-11-17 10:39:08 -0300

    refactor: add helper for controler

--- src/controllers/create-user.js
+++ src/controllers/create-user.js
@@ -1,4 +1,6 @@
 import { CreateUserUseCase } from "../use-cases/create-user.js";
+import validator from "validator";
+import { badRequest, created, serverError } from "./helpers.js";
 
 export class CreateUserController {
   async execute(httpRequest) {
@@ -8,28 +10,25 @@ export class CreateUserController {
 
       for (const field of requiredFields) {
         if (!params[field] || params[field].trim().length === 0) {
-          return {
-            statusCode: 400,
-            body: {
-              errorMessage: `Missing param: ${field}`,
-            },
-          };
+          return badRequest({ message: `Missing or empty field: ${field}` });
         }
+      }
+
+      if (params.password.length < 6) {
+        return badRequest({
+          message: "Password must be at least 6 characters long",
+        });
+      }
+
+      if (!validator.isEmail(params.email)) {
+        return badRequest({ message: "Invalid email format" });
       }
 
       const createUserUseCase = new CreateUserUseCase();
       const createdUser = await createUserUseCase.execute(params);
-      return {
-        statusCode: 201,
-        body: createdUser,
-      };
+      return created(createdUser);
     } catch (error) {
-      return {
-        statusCode: 500,
-        body: {
-          errorMessage: "Internal server error",
-        },
-      };
+      return serverError();
     }
   }
 }
