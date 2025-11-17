d98c0e9b457690ad876a5593a67a6f83f0cacc6f
Geovanni Magnani <magnanigeovanni@gmail.com>
2025-11-17 09:33:29 -0300

    feat: add create user use case

---
+++ src/use-cases/create-user.js
@@ -0,0 +1,24 @@
+import { v4 as uuidv4 } from "uuid";
+import bcrypt from "bcrypt";
+import { PostgresCreateUserRepository } from "../repositories/postgres/create-user";
+
+export class CreateUserUseCase {
+  async execute(createUserParams) {
+    const userId = uuidv4();
+
+    const hashedPassword = await bcrypt.hash(createUserParams.password, 10);
+
+    const newUser = {
+      id: userId,
+      first_name: createUserParams.first_name,
+      last_name: createUserParams.last_name,
+      email: createUserParams.email,
+      password: hashedPassword,
+    };
+
+    const postgresCreateUserRepository = new PostgresCreateUserRepository();
+
+    const createdUser = await postgresCreateUserRepository.execute(newUser);
+    return createdUser;
+}
+}
