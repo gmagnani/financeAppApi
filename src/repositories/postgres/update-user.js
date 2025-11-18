47bba9ec3f39234414e5330e1f4d582bf8b8001d
Geovanni Magnani <magnanigeovanni@gmail.com>
2025-11-18 11:05:35 -0300

    feat: add update user repository

---
+++ src/repositories/postgres/update-user.js
@@ -0,0 +1,16 @@
+import { PostgresHelper } from "../../db/postgres/helper.js";
+
+export class PostgresUpdateUserRepository {
+    async execute(userId, updateUserParams) {
+        const fields = [];
+        const values = [];
+        Object.keys(updateUserParams).forEach((key) => {
+            fields.push(`${key} = $${values.length + 1}`);
+            values.push(updateUserParams[key]);
+        });
+        values.push(userId);
+        const updateQuery = `UPDATE users SET ${fields.join(", ")} WHERE id = $${values.length} RETURNING *;`;
+        const updatedUser = await PostgresHelper.query(updateQuery, values);
+        return updatedUser[0];
+    }
+}
