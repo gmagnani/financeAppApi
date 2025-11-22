54dd06f491efc7c7e30d79b4d8fba6218e0917ab
Geovanni Magnani <magnanigeovanni@gmail.com>
2025-11-22 10:29:00 -0300

    feat: add create transaction repository

---
+++ src/repositories/postgres/transaction/create-transaction.js
@@ -0,0 +1,18 @@
+import { PostgresHelper } from "../../../db/postgres/helper";
+
+export class PostgresCreateTransactionRepository {
+    async execute(createTransactionParams) {
+        const createdTransaction = await PostgresHelper.query(
+            "INSERT INTO transactions (id, user_id, title, date, amount, type) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+            [
+                createTransactionParams.id,
+                createTransactionParams.user_id,
+                createTransactionParams.title,
+                createTransactionParams.date,
+                createTransactionParams.amount,
+                createTransactionParams.type,
+            ]
+        );
+        return createdTransaction[0];
+    }
+}
