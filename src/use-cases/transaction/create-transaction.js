671fbf96f96d36d989ea81ed83a7dd44ccc48f96
Geovanni Magnani <magnanigeovanni@gmail.com>
2025-11-22 13:15:17 -0300

    feat: add create transaction use case

---
+++ src/use-cases/transaction/create-transaction.js
@@ -0,0 +1,29 @@
+import { v4 as uuidv4 } from "uuid";
+import { UserNotFoundError } from "../../errors/user.js";
+
+export class CreateTransactionUseCase {
+    constructor(createTransactionRepository, getUserByIdRepository) {
+        this.createTransactionRepository = createTransactionRepository;
+        this.getUserByIdRepository = getUserByIdRepository;
+    }
+
+    async execute(createTransactionParams) {
+        const user = await this.getUserByIdRepository.execute(
+            createTransactionParams.userId
+        );
+
+        if (!user) {
+            throw new UserNotFoundError();
+        }
+
+        const transactionId = uuidv4();
+
+        const createdTransaction =
+            await this.createTransactionRepository.execute({
+                ...createTransactionParams,
+                id: transactionId,
+            });
+
+        return createdTransaction;
+    }
+}
