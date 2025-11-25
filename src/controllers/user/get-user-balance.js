a4c28b11343bd2b9e51c3fd25976218bca40cb9e
Geovanni Magnani <magnanigeovanni@gmail.com>
2025-11-25 16:50:18 -0300

    feat: add get user balance controller

---
+++ src/controllers/user/get-user-balance.js
@@ -0,0 +1,33 @@
+import { UserNotFoundError } from "../../errors/user";
+import {
+    checkIfIdIsValid,
+    invalidIdResponse,
+    ok,
+    serverError,
+    notFoundResponse,
+} from "../helpers/index.js";
+
+export class GetUserBalanceController {
+    constructor(getUserBalanceUseCase) {
+        this.getUserBalanceUseCase = getUserBalanceUseCase;
+    }
+    async execute(httpRequest) {
+        try {
+            const userId = httpRequest.params.userId;
+            const isIdValid = checkIfIdIsValid(userId);
+            if (!isIdValid) {
+                return invalidIdResponse();
+            }
+            const balance = await this.getUserBalanceUseCase.execute({
+                userId,
+            });
+            return ok(balance);
+        } catch (error) {
+            if (error instanceof UserNotFoundError) {
+                return notFoundResponse();
+            }
+            console.log(error);
+            return serverError();
+        }
+    }
+}
